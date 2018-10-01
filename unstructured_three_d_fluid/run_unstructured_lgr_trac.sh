9b11c0140089bbd5a3ae728e2501e76d5fe984ed
Ray <raymonwhite42+gitlab@gmail.com>
2018-10-01 04:27:28 +0100

    Finished re-writing script

---
+++ unstructured_three_d_fluid/run_unstructured_lgr_trac.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+RUNCOMMAND="mpirun -np 1"
+PROGRAM="unstructured_lgr_trac"
+
+BRICK="--use_brick"
+LINSOLVER="--use_iterative_lin_solver"
+TRILINOS="--use_trilinos"
+NSSOLVER="--use_lsc"
+FSOLVER="--use_amg_for_f --use_amg2v22_for_f"
+PSOLVER="--use_amg_for_p"
+VISCTERM="--use_stress_div"
+RE="--re 500.0"
+
+# Concatenate problem parameters
+PROBPARAM="${BRICK}"
+PROBPARAM="${PROBPARAM} ${LINSOLVER} ${TRILINOS}"
+PROBPARAM="${PROBPARAM} ${NSSOLVER} ${FSOLVER} ${PSOLVER}"
+PROBPARAM="${PROBPARAM} ${VISCTERM} ${RE}"
+
+# Stuff to help document solutions
+SOLNLABEL="--doc_label fluid_soln"
+DOCNUM="--doc_num 1"
+TETGENLABEL="--tetgen_label tetgen_notaligned_short/fsi_bifurcation_fluid"
+TETGENNUM="--tetgen_num 1"
+
+# Concatenate the documentation parameters
+DOCPARAM="${SOLNLABEL} ${DOCNUM} ${TETGENLABEL} ${TETGENNUM}"
+
+
+# Time stepping parameters
+DOUNSTEADY="--do_unsteady"
+TSTART="--tstart 0.0"
+TEND="--tend 1.0"
+DT="--dt 0.025" 
+
+# Concatenate the time stepping parameters
+TIMEPARAM="${DOUNSTEADY} ${TSTART} ${TEND} ${DT}"
+
+${RUNCOMMAND} ./${PROGRAM} ${PROBPARAM} ${DOCPARAM} ${TIMEPARAM}
+
+#mpirun -np 1 ./unstructured_lgr_trac --use_brick --use_trilinos --use_iterative_lin_solver --use_lsc --use_amg_for_f --use_amg2v22_for_f --use_amg_for_p  --use_stress_div --re 500.0    --doc_label fluid_soln --tetgen_label tetgen_notaligned_short/fsi_bifurcation_fluid  --tetgen_num 2 --doc_num 2 --do_unsteady --tstart 0.0 --tend 1.0 --dt 0.025  
+#
+#
+#mpirun -np 1 ./unstructured_lgr_trac --use_brick --use_trilinos --use_iterative_lin_solver --use_lsc --use_amg_for_f --use_amg2v22_for_f --use_amg_for_p  --use_stress_div --re 500.0    --doc_label fluid_soln --tetgen_label tetgen_notaligned_short/fsi_bifurcation_fluid  --tetgen_num 3 --doc_num 3 --do_unsteady --tstart 0.0 --tend 1.0 --dt 0.025  
+#
+#
+#mpirun -np 1 ./unstructured_lgr_trac --use_brick --use_trilinos --use_iterative_lin_solver --use_lsc --use_amg_for_f --use_amg2v22_for_f --use_amg_for_p  --use_stress_div --re 500.0    --doc_label fluid_soln --tetgen_label tetgen_notaligned_short/fsi_bifurcation_fluid  --tetgen_num 4 --doc_num 4 --do_unsteady --tstart 0.0 --tend 1.0 --dt 0.025  
+#
+#
+
+
+#mpirun -np 1 ./unstructured_lgr_trac --use_brick --use_trilinos --use_iterative_lin_solver --use_lsc --use_amg_for_f --use_amg2v22_for_f --use_amg_for_p  --use_stress_div --re 500.0    --doc_label fluid_soln --tetgen_label tetgen_notaligned_short/fsi_bifurcation_fluid  --tetgen_num 7 --doc_num 2 --do_unsteady --tstart 0.0 --tend 1.0 --dt 0.05  
+#mpirun -np 1 ./unstructured_lgr_trac --use_brick --use_trilinos --use_iterative_lin_solver --use_lsc --use_amg_for_f --use_amg2v22_for_f --use_amg_for_p  --use_stress_div --re 500.0    --doc_label fluid_soln --tetgen_label tetgen_notaligned_short/fsi_bifurcation_fluid  --tetgen_num 7 --doc_num 3 --do_unsteady --tstart 0.0 --tend 1.0 --dt 0.025  
+#mpirun -np 1 ./unstructured_lgr_trac --use_brick --use_trilinos --use_iterative_lin_solver --use_lsc --use_amg_for_f --use_amg2v22_for_f --use_amg_for_p  --use_stress_div --re 500.0    --doc_label fluid_soln --tetgen_label tetgen_notaligned_short/fsi_bifurcation_fluid  --tetgen_num 7 --doc_num 4 --do_unsteady --tstart 0.0 --tend 1.0 --dt 0.0125  
